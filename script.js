f2ae06b6884244ddff3cba5a368f1314fef19ede
Piotr Obara <pobara@o2.pl>
2024-02-20 17:43:44 +0100

    Improvement of tasks functionality. Addition of the ability to change the order within columns using drag and drop.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const addTaskButton = document.querySelector(".add-task-btn");
 const taskInput = document.querySelector(".task-input");
 const todoColumn = document.getElementById("todo-column");
 const columns = document.querySelectorAll(".column");
-const dragelement = document.getElementById("dragelement");
 
 // Funkcja tworzenia nowego zadania
 function createTask(taskText) {
@@ -26,17 +25,6 @@ function createTask(taskText) {
     return taskElement;
 }
 
-// Obsługa przeciągania elementów
-addTaskButton.addEventListener("click", () => {
-    const taskText = taskInput.value.trim();
-    if (taskText !== "") {
-        const taskElement = createTask(taskText);
-        todoColumn.appendChild(taskElement);
-        taskInput.value = "";
-        makeTaskDraggable(taskElement);
-    }
-});
-
 // Funkcja, która sprawia, że element jest przeciągalny
 function makeTaskDraggable(taskElement) {
     taskElement.draggable = true;
@@ -50,7 +38,7 @@ function makeTaskDraggable(taskElement) {
     });
 }
 
-// Obsługa upuszczania elementów
+// Obsługa przeciągania i upuszczania elementów
 columns.forEach((zone) => {
     zone.addEventListener("dragover", (e) => {
         e.preventDefault();
@@ -60,5 +48,47 @@ columns.forEach((zone) => {
         if (draggingTask && draggingTask.parentNode !== zone) {
             zone.appendChild(draggingTask);
         }
+        
+        const mouseY = e.clientY;
+        const bottomTask = insertAboveTask(zone, mouseY);
+        const curTask = document.querySelector(".is-dragging");
+
+        if (!bottomTask) {
+            zone.appendChild(curTask);
+        } else {
+            zone.insertBefore(curTask, bottomTask);
+        }
     });
 });
+
+// Obsługa dodawania nowego zadania
+addTaskButton.addEventListener("click", () => {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        const taskElement = createTask(taskText);
+        todoColumn.appendChild(taskElement);
+        taskInput.value = "";
+        makeTaskDraggable(taskElement);
+    }
+});
+
+// Funkcja do określania pozycji myszy względem zadań
+const insertAboveTask = (zone, mouseY) => {
+    const els = zone.querySelectorAll(".task:not(.is-dragging)");
+
+    let closestTask = null;
+    let closestOffset = Number.NEGATIVE_INFINITY;
+
+    els.forEach((task) => {
+        const { top } = task.getBoundingClientRect();
+
+        const offset = mouseY - top;
+
+        if (offset < 0 && offset > closestOffset) {
+            closestOffset = offset;
+            closestTask = task;
+        }
+    });
+
+    return closestTask;
+};
