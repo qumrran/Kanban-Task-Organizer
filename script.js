2d83894b5a6d4c70257cf80f1a8a02b3b11e2999
Piotr Obara <pobara@o2.pl>
2024-02-20 06:31:31 +0100

    Adding drag-and-drop functionality between columns, SCSS improvements

--- script.js
+++ script.js
@@ -4,29 +4,61 @@ const todoColumn = document.getElementById("todo-column");
 const columns = document.querySelectorAll(".column");
 const dragelement = document.getElementById("dragelement");
 
-addTaskButton.addEventListener("click", function () {
+// Funkcja tworzenia nowego zadania
+function createTask(taskText) {
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+
+    const taskContent = document.createElement("p");
+    taskContent.textContent = taskText;
+    taskContent.classList.add("task-content");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="fas fa-minus-circle"></i>';
+    deleteButton.classList.add("delete-button");
+    deleteButton.addEventListener("click", () => {
+        taskElement.remove();
+    });
+
+    taskElement.appendChild(taskContent);
+    taskElement.appendChild(deleteButton);
+
+    return taskElement;
+}
+
+// Obsługa przeciągania elementów
+addTaskButton.addEventListener("click", () => {
     const taskText = taskInput.value.trim();
     if (taskText !== "") {
-        const taskElement = document.createElement("div");
-        taskElement.classList.add("task");
-
-        const taskContent = document.createElement("p");
-        taskContent.textContent = taskText;
-        taskContent.classList.add("task-content");
-
-        const deleteButton = document.createElement("button");
-        deleteButton.innerHTML = '<i class="fas fa-minus-circle"></i>'
-        deleteButton.classList.add("delete-button");
-        deleteButton.addEventListener("click", function() {
-            taskElement.remove();
-        });
-
-        taskElement.appendChild(taskContent);
-        taskElement.appendChild(deleteButton);
-
+        const taskElement = createTask(taskText);
         todoColumn.appendChild(taskElement);
         taskInput.value = "";
+        makeTaskDraggable(taskElement);
     }
 });
 
+// Funkcja, która sprawia, że element jest przeciągalny
+function makeTaskDraggable(taskElement) {
+    taskElement.draggable = true;
 
+    taskElement.addEventListener("dragstart", () => {
+        taskElement.classList.add("is-dragging");
+    });
+
+    taskElement.addEventListener("dragend", () => {
+        taskElement.classList.remove("is-dragging");
+    });
+}
+
+// Obsługa upuszczania elementów
+columns.forEach((zone) => {
+    zone.addEventListener("dragover", (e) => {
+        e.preventDefault();
+
+        const draggingTask = document.querySelector(".is-dragging");
+
+        if (draggingTask && draggingTask.parentNode !== zone) {
+            zone.appendChild(draggingTask);
+        }
+    });
+});
